a4f3d2e34e27a775c3fc1e78ac6a068dcbc5f32a
liamoreganstu-eng <liam.oregan.stu@cedarpark.org>
2026-05-14 09:20:08 -0700

    fun

--- platformGame.py
+++ platformGame.py
@@ -2,26 +2,32 @@ import pgzrun
 
 # Variables
 TITLE = "Platform Game"
-WIDTH = 800
-HEIGHT = 500
-player = Rect((100, 400), (40, 40))
-velocity_y = 10
-gravity = 1
+WIDTH = 1800
+HEIGHT = 1000
+player = Rect((100, 400), (30, 30))
+velocity_y = 1
+gravity = .6
 on_ground = False
 lava = Rect((350, 450), (100, 20))
 platforms = [
-    Rect((0, 470), (800, 30)),
+    Rect((0, 470), (980, 30)),
+    Rect((1300, 470), (1800, 30)),
     Rect((200, 380), (150, 20)),
     Rect((450, 300), (150, 20)),
-    Rect((650, 220), (100, 20))
+    Rect((650, 220), (100, 20)),
+    Rect((300, 220), (100, 20)),
 ]
 coins = [
     Rect((250, 340), (20, 20)),
     Rect((500, 260), (20, 20)),
-    Rect((690, 180), (20, 20))
+    Rect((690, 180), (20, 20)),
+    Rect((350, 180), (20, 20)),
 ]
-
+  
+goal = Rect((730, 420), (40, 50))
+game_won = False
 score = 0
+death = 0
 
 def draw():
     screen.clear()
@@ -35,9 +41,12 @@ def draw():
     
     screen.draw.text(f"Score: {score}", (10, 10), fontsize=30, color="white")
     screen.draw.filled_rect(lava, "red")
+    screen.draw.filled_rect(goal, "green")
+    if game_won:
+        screen.draw.text("You Win!", center=(400, 250), fontsize=60, color="yellow")
 
 def update():
-    global velocity_y, on_ground, score
+    global velocity_y, on_ground, score, game_won
     
     velocity_y += gravity
     player.y += velocity_y
@@ -59,7 +68,7 @@ def update():
         velocity_y = 0
         on_ground = True
 
-    if keyboard.space and on_ground:
+    if keyboard.up and on_ground:
         velocity_y = -15
         on_ground = False
 
@@ -79,4 +88,9 @@ def update():
         player.y = 400
         velocity_y = 0
 
+    if player.colliderect(goal) and score >= 3:
+        game_won = True
+
+    if game_won:
+        screen.draw.text("You Win!", center=(400, 250), fontsize=60, color="yellow")
 pgzrun.go()
